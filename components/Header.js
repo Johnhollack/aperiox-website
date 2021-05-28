1d587f10f64aaf1b800cc789c0683031e6797fd2
johnhollack <adejumolekan@gmail.com>
2021-05-28 18:12:08 +0100

    develop menu button

--- components/Header.js
+++ components/Header.js
@@ -18,7 +18,7 @@ function Header() {
         <header className="flex sm:flex-row my-3 justify-between items-center h-auto w-screen">
             <img src="/logo.svg" alt="Apeirox Logo" className="ml-10 h-10 sm:h-12 object-contain" />
 
-            <div className="flex flex-grow items-center justify-end sm:justify-evenly w-xs sm:max-w-xs ">
+            <div className="flex flex-grow items-center justify-end sm:justify-evenly w-xs sm:max-w-xs mr-10 sm:mr-0">
                 <HeaderItem icon={faFacebook} url="https://facebook.com/apeiroxcom" />
                 <HeaderItem icon={faTwitter} url="https://twitter.com/apeiroxcom" />
                 <HeaderItem icon={faInstagram} url="https://instagram.com/apeiroxcom" />
